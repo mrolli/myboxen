526b3efc28f7a0b61cdb41c132321760bb010246
Michael Rolli <michael@rollis.ch>
2017-02-01 22:39:43 +0100

    Removed stuff not relevant.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,6 @@
 require boxen::environment
 require homebrew
-require gcc
+#require gcc
 
 Exec {
   group       => 'staff',
@@ -53,26 +53,23 @@ Homebrew::Formula <| |> -> Package <| |>
 
 node default {
   # core modules, needed for most things
-  include dnsmasq
+  #include dnsmasq
   include git
-  include hub
-  include nginx
+  #include hub
+  #include nginx
 
   # fail if FDE is not enabled
   if $::root_encrypted == 'no' {
     fail('Please enable full disk encryption and try again')
   }
 
-  # node versions
-  nodejs::version { '0.8': }
-  nodejs::version { '0.10': }
-  nodejs::version { '0.12': }
-
   # default ruby versions
-  ruby::version { '1.9.3': }
-  ruby::version { '2.0.0': }
-  ruby::version { '2.1.8': }
-  ruby::version { '2.2.4': }
+  #ruby::version { '1.9.3': }
+  #ruby::version { '2.0.0': }
+  #ruby::version { '2.1.8': }
+  #ruby::version { '2.2.4': }
+  ruby::version { '2.2.6': }
+  ruby::version { '2.3.3': }
 
   # common, useful packages
   package {
@@ -83,8 +80,8 @@ node default {
     ]:
   }
 
-  file { "${boxen::config::srcdir}/our-boxen":
-    ensure => link,
-    target => $boxen::config::repodir
-  }
+  #file { "${boxen::config::srcdir}/our-boxen":
+  #  ensure => link,
+  #  target => $boxen::config::repodir
+  #}
 }
